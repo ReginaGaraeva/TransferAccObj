0fa6b55da8b9a57fe184e764ad232fd53542bbc4
ReginaGaraeva <garaeva.regina9@gmail.com>
2014-07-15 14:12:03 +0600

    Добавлен класс AccountingObjectModel

---
+++ ObjectTransferWCF/ObjectTransferWCF/Models/AccountingObjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObjectTransferWCF.Models
+{
+    public class AccountingObjectModel
+    {
+        public string InventaryNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public DateTime DeprecationDate { get; set; }
+        public string Owner { get; set; }
+        public bool Deleted { get; set; }
+    }
+}

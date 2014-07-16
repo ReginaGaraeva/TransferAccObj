63dae184717a1d3f9fc8a75897230031073a56e9
ReginaGaraeva <garaeva.regina9@gmail.com>
2014-07-16 11:55:43 +0600

    Добавлен интерфейс ILogService
    Интерфейс описывает методы для работы с логами. В перспективе.

---
+++ ObjectTransferWCF/ObjectTransferWCF/Interfaces/ILogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTransferWCF.Interfaces
+{
+    interface ILogService
+    {
+        void WriteInfo(string message);
+        void WriteError(string message);
+        List<ILogService> GetLogsByDate(DateTime dateFrom, DateTime dateEnd = null);
+        bool DeleteLogById(int id);
+    }
+}

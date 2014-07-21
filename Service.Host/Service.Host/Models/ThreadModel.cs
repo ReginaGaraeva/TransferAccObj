ceb06245160fbf96096931234d8ddd688a0b722e
ReginaGaraeva <garaeva.regina9@gmail.com>
2014-07-21 12:04:50 +0600

    п

---
+++ Service.Host/Service.Host/Models/ThreadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Host.Services;
+using Service.Host.ServiceReference1;
+using System.Threading;
+
+namespace Service.Host.Models
+{
+    class ThreadModel
+    {
+        public ObjectTransferServiceClient service;
+        public Thread thread;
+    }
+}
---
+++ Service.Host/Service.Host/Services/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Host.Services;
+using Service.Host.ServiceReference1;
+using System.Threading;
+
+namespace Service.Host.Services
+{
+    class Test
+    {
+        private Generator generator;
+        List<ObjectTransferServiceClient> services;
+        List<Thread> threads;
+
+        public void Run(int maxServicesCount, int operationsCount)
+        {
+        }
+
+        public void Stop()
+        {
+            foreach (var thread in threads)
+                thread.Abort();
+        }
+
+        public void StartService()
+        {
+
+        }
+
+        public void StopService()
+        {
+
+        }
+    }
+}

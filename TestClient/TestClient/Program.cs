dbbcb55181ee71ba8a82b13bbf44dfb308a5a32c
ReginaGaraeva <garaeva.regina9@gmail.com>
2014-07-22 14:35:00 +0600

    xfgh

---
+++ TestClient/TestClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var service = new TestClient.ServiceReference1.Service1Client())
+            {
+                Console.WriteLine(service.GetData(3));
+                Console.ReadLine();
+                service.Close();
+            }
+        }
+    }
+}
